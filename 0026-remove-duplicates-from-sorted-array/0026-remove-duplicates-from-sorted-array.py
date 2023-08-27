e69424706981f0ee29eacc7d6245dad601cd5284
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-08-27 15:12:10 -0400

    Time: 94 ms (55.95%), Space: 18 MB (51.63%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -11,10 +11,27 @@
  #time compexity is better 0 (n log n ) but space :0(n)
 
 
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        nums[:] =  sorted(set(nums))
-        return len(nums)
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         nums[:] =  sorted(set(nums))
+#         return len(nums)
 
 
 #time compexity is better 0 (n log n ) but space :0(n)
+
+
+#Two pointer 
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow ,fast = 0,1
+        while fast in range (len(nums)):
+            if nums[slow] == nums[fast]:
+                fast +=1
+            else :
+                nums[slow +1 ] = nums[fast]
+                slow +=1
+                fast +=1
+        return slow +1
+
+
