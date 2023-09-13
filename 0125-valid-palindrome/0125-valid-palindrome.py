b72bda3f44a8368e1df52e24c73fea4f619a3105
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-09-13 00:53:23 -0400

    Time: 57 ms (38.94%), Space: 17.1 MB (85.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l,r = 0, len(s) - 1
-
-        while l< r:
-            while l< r and not self.alphanum(s[l]):
-                l += 1
-            while l< r and not self.alphanum(s[r]):
-                r -=1
-            if s[l].lower() != s[r].lower():
+               
+        i, j = 0, len(s) - 1
+              
+        while i < j:          
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            elif s[i].lower() != s[j].lower():
                 return False
-            l += 1
-            r -= 1
+            else:
+                i, j = i + 1, j - 1
         return True
 
+        
 
-
-    def alphanum(self,c):
-        return(
-            ord("A") <= ord(c) <= ord("Z")
-            or ord("a") <= ord(c) <= ord("z")
-            or ord("0") <= ord(c) <= ord("9")
-        )
-
+        # i starts at beginning of s and j starts at the end  
+        # While i is still before j  
+        # If i is not an alpha-numeric character then advance i  
+        # If j is not an alpha-numeric character then advance j
+        # Both i and j are alpha-numeric characters at this point so if they do not match return the fact that input was non-palendromic
+        # Otherwise characters matched and we should look at the next pair of characters
+        # The entire stirng was verified and we return the fact that the input string was palendromic   
