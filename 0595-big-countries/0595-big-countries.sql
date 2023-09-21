b1c21e78728e89eeffc938605484e3020e8692a8
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-09-21 18:52:40 -0400

    Time: 426 ms (97.01%), Space: 0B (100%) - LeetHub

---
+++ 0595-big-countries/0595-big-countries.sql
@@ -0,0 +1,12 @@
+# order dal ke
+
+select name,population, area from world
+where area >= 3000000 or population >= 25000000
+order by name asc
+
+
+#without order
+
+# Select name ,population, area
+# from world
+# where area >=3000000 OR POPULATION >= 25000000
