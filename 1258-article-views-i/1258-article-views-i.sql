63318cc99a4d762ea089b586e61ea2b8452ed8c9
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-09-22 23:48:15 -0400

    Time: 694 ms (96.45%), Space: 0B (100%) - LeetHub

---
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+
+
+select distinct author_id as id
+from views
+where author_id = viewer_id
+order by id
+
+
+
+
+
+
+
+
+
+
+
+
+# select distinct author_id as id
+# from views
+# where author_id = viewer_id 
+# order by id
+
+
