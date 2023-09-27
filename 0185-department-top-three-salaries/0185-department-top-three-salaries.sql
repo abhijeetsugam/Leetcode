cb6135a6d035a24df6c9fced8e0359a988391c60
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-09-27 05:18:25 -0400

    Time: 1527 ms (95.92%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,8 +1,24 @@
-SELECT Department, employee, salary FROM (
-    SELECT d.name AS Department
-        , e.name AS employee
-        , e.salary
-        , DENSE_RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS salary_rank
-    FROM Employee e left JOIN Department d ON e.DepartmentId= d.Id
-) t WHERE t.salary_rank <= 3
+# SELECT Department, employee, salary FROM (
+#     SELECT d.name AS Department
+#         , e.name AS employee
+#         , e.salary
+#         , DENSE_RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS salary_rank
+#     FROM Employee e left JOIN Department d ON e.DepartmentId= d.Id
+# ) t WHERE t.salary_rank <= 3
 
+
+
+select
+    d.name department,
+    e1.name employee,
+    e1.salary salary
+from
+    employee e1
+    join employee e2
+    join department d on e1.departmentid = e2.departmentid
+    and e1.salary <= e2.salary
+    and d.id = e2.departmentid
+group by
+    d.name,  e1.salary, e1.name
+having
+    count(distinct(e2.salary)) <= 3
