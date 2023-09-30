0b3109b25513636d5dc31d2dea6dc2c7853c0e2f
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-09-30 16:59:58 -0400

    Time: 576 ms (96.3%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,3 +1,7 @@
-select *
-from patients
-where conditions like '% diab1%' or conditions like 'diab1%'
+# select *
+# from patients
+# where conditions like '% diab1%' or conditions like 'diab1%'
+
+
+SELECT * FROM Patients
+WHERE conditions rlike '\\bDIAB1';
