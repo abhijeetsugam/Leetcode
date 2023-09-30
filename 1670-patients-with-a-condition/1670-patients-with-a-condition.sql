517a4ba4d5b6cf4734c160657d38f2248fec962a
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-09-30 17:01:56 -0400

    Time: 597 ms (93.61%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -2,6 +2,9 @@
 # from patients
 # where conditions like '% diab1%' or conditions like 'diab1%'
 
+#1st condition when its 2nd word like in example 4 
+#2nd condition when its 1st word like in example 3 
+
 
 SELECT * FROM Patients
 WHERE conditions rlike '\\bDIAB1';
