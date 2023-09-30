45a71b1c314022d3d0c8758345c6e66501ad55ce
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-09-30 17:45:12 -0400

    Time: 1513 ms (75.92%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,2 +1,11 @@
-delete p1 from person p1,person p2 
-where p1.email=p2.email and p1.id > p2.id
+
+
+# delete p1 
+# from person p1,person p2 
+# where p1.email=p2.email and p1.id > p2.id
+
+
+DELETE p1 
+FROM person p1
+JOIN person p2 ON p1.email = p2.email 
+WHERE p1.id > p2.id;
