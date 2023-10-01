43748ec80f22ae5136607be519393ae52b006718
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-10-01 03:59:08 -0400

    Time: 1237 ms (97.18%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,14 +1,17 @@
 # mine
-# select product_id , ifnull ( round(sum(units * price)/ sum(units), 2), 0) as average_price
-# from Prices p  
-# natural left join unitssold u
-# where purchase_date between start_date and end_date  
-# group by product_id 
 
-# Write your MySQL query statement below
-SELECT p.product_id, IFNULL(round(SUM(p.price*u.units)/sum(u.units),2),0) as average_price
-FROM Prices p 
-LEFT JOIN UnitsSold u
-ON p.product_id = u.product_id AND 
-u.purchase_date BETWEEN p.Start_date and p.end_date
-GROUP BY p.product_id
+select p.product_id , ifnull ( round(sum(units * price)/ sum(units), 2), 0) as average_price
+from Prices p  
+left join unitssold u
+on p.product_id = u.product_id
+and purchase_date between start_date and end_date  
+group by p.product_id 
+
+# yoman
+
+# SELECT p.product_id, IFNULL(round(SUM(p.price*u.units)/sum(u.units),2),0) as average_price
+# FROM Prices p 
+# LEFT JOIN UnitsSold u
+# ON p.product_id = u.product_id AND 
+# u.purchase_date BETWEEN p.Start_date and p.end_date
+# GROUP BY p.product_id
