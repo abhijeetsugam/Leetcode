80a6e6cad9a097a3570a5d82c00a2ba98498879d
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-10-07 19:24:08 -0400

    Time: 1140 ms (66.89%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,11 +1,8 @@
-select employee_id , department_id 
-from Employee e
-where  primary_flag ='Y' or employee_id IN (
-    SELECT employee_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(*) = 1
-)
+# #s1
+
+# select employee_id , department_id 
+# from Employee e
+# where  primary_flag ='Y' 
 
 # union
 
@@ -18,5 +15,29 @@ where  primary_flag ='Y' or employee_id IN (
 #     HAVING COUNT(*) = 1
 # )
 
+# #s2
+
+# select employee_id , department_id 
+# from Employee e
+# where  primary_flag ='Y' or employee_id IN (
+#     SELECT employee_id
+#     FROM Employee
+#     GROUP BY employee_id
+#     HAVING COUNT(*) = 1
+# )
+
+#s3
+
+select employee_id , department_id 
+from Employee e
+where  primary_flag ='Y' 
+
+
+union
+
+select employee_id , department_id 
+from Employee e
+group by employee_id 
+having  count(employee_id) = 1 
 
 
