96535ec3b99e0243f5ef2b4b4aa7a7f963a84b41
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-10-06 22:26:56 -0400

    Time: 970 ms (98.61%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,6 +1,5 @@
 # select  round(sum(if (order_date = customer_pref_delivery_date, 1, 0))/count(* ) *100 , 2 ) as immediate_percentage 
-Select 
-    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+Select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
 from Delivery 
 where ( customer_id , order_date ) in (
 
