a25d8bed581ef5d1558b79823f445af5407c9903
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-10-07 19:17:58 -0400

    Time: 920 ms (96.86%), Space: 0B (100%) - LeetHub

---
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -0,0 +1,18 @@
+select employee_id , department_id 
+from Employee e
+where  primary_flag ='Y' 
+
+union
+
+SELECT employee_id, department_id 
+FROM Employee
+WHERE employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
+)
+
+order by employee_id
+
+
