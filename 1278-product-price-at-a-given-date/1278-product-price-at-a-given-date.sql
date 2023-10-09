2ba99e831c3ea9dcebe832153cbd28229cbe2909
Abhijeet sugam <121594655+abhijeetsugam@users.noreply.github.com>
2023-10-09 15:50:12 -0400

    Time: 890 ms (74.81%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -19,4 +19,4 @@ select product_id , new_price as price from products where (product_id , change_
 union
 
 select product_id , 10 as price from products where product_id not in 
-(select product_id  from products where change_date <= ' 2019-08-16' group by product_id) 
+(select product_id  from products where change_date <= ' 2019-08-16') 
